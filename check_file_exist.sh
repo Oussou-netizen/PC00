65d7fdffe0f81569f865d0c5c0d962d167e8f025
Oussou-netizen <ousmanedjibrilou6@gmail.com>
2025-05-08 00:06:11 +0100

    Create check_file_exist.sh

---
+++ check_file_exist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Script qui vérifie si un fichier existe ou pas
+echo "Entrez le nom du fichier :"
+read fileName
+if [ -f "$fileName" ]; then
+ echo "Le fichier '$filename' existe"
+else 
+ echo "Le fichier '$fileName' n'existe pas"
+fi
