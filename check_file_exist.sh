d86de57c0f1716bcd25b92b01f3a144acfffad26
Oussou-netizen <ousmanedjibrilou6@gmail.com>
2025-05-08 06:32:54 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,8 +3,8 @@
 echo -n "Entrez le nom du fichier : " 
 read fileName
 #La condition if permet de [ -f filename ] permet de verifier si le le fichier est regulier et qu'il existe
-if [-f"$fileName"]; then
-echo "Le fichier '$fileName' existe !"
+if [ -f"$fileName" ]; then
+   echo "Le fichier '$fileName' existe !"
 else 
-echo "Le fichier '$fileName' existe pas !"
+   echo "Le fichier '$fileName' n'existe pas !"
 fi
